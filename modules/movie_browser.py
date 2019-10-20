3d2192ce4606df3fcc7ee36f93084525325e9fd4
Robert Vari <mail.robertvari@gmail.com>
2019-10-20 15:15:12 +0200

    Modules: Movie browser Movie details

---
+++ modules/movie_browser.py
@@ -0,0 +1,10 @@
+from PySide2.QtWidgets import QWidget, QVBoxLayout
+
+class MovieBrowser(QWidget):
+    def __init__(self):
+        super(MovieDetails, self).__init__()
+
+
+class SearchBar(QWidget):
+    def __init__(self):
+        super(MovieDetails, self).__init__()
---
+++ modules/movie_details.py
@@ -0,0 +1,5 @@
+from PySide2.QtWidgets import QWidget, QVBoxLayout
+
+class MovieDetails(QWidget):
+    def __init__(self):
+        super(MovieDetails, self).__init__()
