89b98ce2e53ee67e38738a06397fc446d45427f3
Robert Vari <mail.robertvari@gmail.com>
2019-11-03 15:54:56 +0100

    Client object

---
+++ objects/database.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+from bson import ObjectId
+
+class Client():
+    def __init__(self):
+        client = MongoClient("localhost", 27017)
+        db = client["MovieLibrary"]
+        self.collection = db["movies"]
+
+    def insert(self, data):
+        return self.collection.insert_one(data).inserted_id
+
+    def update(self, data):
+        self.collection.update_one({"_id":data.get("_id")}, {"$set":data})
+
+    def find_all_movies(self):
+        return self.collection.find()
+
+    def find_by_title(self, title):
+        return self.collection.find_one({"title":title})
+
+    def find_by_id(self, _id):
+        return self.collection.find_one({"_id": _id})
+
+    def delete(self, _id):
+        self.collection.delete_one({"_id":_id})
+
+
+if __name__ == '__main__':
+    client = Client()
+    movie_data = client.find_by_title("Star wars")
+    movie_data["title"] = "Jaws"
+    client.update(movie_data)
