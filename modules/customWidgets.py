b8d57208a33008b16925860104ae19009de5a537
Robert Vari <mail.robertvari@gmail.com>
2019-11-16 17:16:20 +0100

    Details view style

--- modules/customWidgets.py
+++ modules/customWidgets.py
@@ -119,12 +119,39 @@ class IconButton(QWidget):
         self.painter.drawPixmap(rect, self.pixmap)
 
 
+class BackdropImageWidget(QWidget):
+    def __init__(self):
+        super(BackdropImageWidget, self).__init__()
+        self.backdrop_image = ""
+        self.painter = QPainter()
+
+        self.pixmap = QPixmap(self.backdrop_image)
+        self.fill_brush = QBrush(QColor(0, 0, 0, 210))
+
+    def set_backdrop_image(self, image_path):
+        self.backdrop_image = image_path
+        self.pixmap.load(image_path)
+
+    def paintEvent(self, event):
+        self.painter.begin(self)
+        self.draw()
+        self.painter.end()
+
+    def draw(self):
+        rect = self.rect()
+
+        self.painter.drawPixmap(rect, self.pixmap)
+
+        self.painter.setBrush(self.fill_brush)
+        self.painter.setPen(Qt.NoPen)
+        self.painter.drawRect(rect)
+
+
 if __name__ == '__main__':
     import sys
 
-    from utilities.static_utils import get_static
-
     app = QApplication(sys.argv)
-    win = IconButton(get_static("sort_AZ.png"))
+    win = BackdropImageWidget()
+    win.set_backdrop_image(r"C:\Users\Robert\Movie_Library\1573910895_backdrop.jpg")
     win.show()
     app.exec_()
--- modules/movie_details.py
+++ modules/movie_details.py
@@ -1,29 +1,59 @@
-from PySide2.QtWidgets import QWidget, QVBoxLayout, QGroupBox, QPushButton, QLabel
-from PySide2.QtCore import Signal
+from PySide2.QtWidgets import QWidget, QVBoxLayout, QGroupBox, QPushButton, QLabel, \
+    QHBoxLayout, QSizePolicy, QSpacerItem
+from PySide2.QtCore import Signal, Qt
 
-class MovieDetails(QWidget):
+from .customWidgets import BackdropImageWidget, Button
+
+class MovieDetails(BackdropImageWidget):
     close_details = Signal()
 
     def __init__(self):
         super().__init__()
         self.setVisible(False)
 
-        main_layout = QVBoxLayout(self)
+        main_layout = QHBoxLayout(self)
 
         self.poster = QLabel()
-        self.description = QLabel()
-        self.rating = QLabel()
-        self.title = QLabel()
-        self.release_date = QLabel()
-
+        self.poster.setAlignment(Qt.AlignTop|Qt.AlignLeft)
+        self.poster.setMaximumWidth(320)
         main_layout.addWidget(self.poster)
-        main_layout.addWidget(self.description)
-        main_layout.addWidget(self.rating)
-        main_layout.addWidget(self.title)
-        main_layout.addWidget(self.release_date)
 
-        close_btn = QPushButton("Close")
-        main_layout.addWidget(close_btn)
+
+        details_layout = QVBoxLayout()
+        details_layout.setAlignment(Qt.AlignTop)
+        main_layout.addLayout(details_layout)
+
+        title_layout = QHBoxLayout()
+        details_layout.addLayout(title_layout)
+        self.title = QLabel()
+        self.title.setObjectName("movie_title")
+
+        self.release_date = QLabel()
+        self.release_date.setObjectName("small_text")
+
+        self.rating = QLabel()
+        self.rating.setObjectName("small_text")
+
+        overview_lbl = QLabel("Overview")
+        overview_lbl.setObjectName("subtitle")
+
+        self.description = QLabel()
+        self.description.setWordWrap(True)
+
+        title_layout.addWidget(self.title)
+        title_layout.addWidget(self.release_date)
+
+        title_layout.addItem(QSpacerItem(10,10, QSizePolicy.Expanding, QSizePolicy.Minimum))
+
+        close_btn = Button("Close")
+        title_layout.addWidget(close_btn)
+
+        details_layout.addWidget(self.rating)
+
+        details_layout.addWidget(overview_lbl)
+        details_layout.addWidget(self.description)
+
+
         close_btn.clicked.connect(self.close_action)
 
     def set_movie(self, movie):
@@ -31,8 +61,11 @@ class MovieDetails(QWidget):
 
         self.poster.setPixmap(movie.poster)
         self.title.setText(movie.title)
-        self.rating.setText(str(movie.rating))
-        self.release_date.setText(movie.release_date)
+        self.rating.setText( f"Raing: {movie.rating}")
+        self.release_date.setText(f" ({movie.release_date})"  )
+        self.description.setText(movie.description)
+
+        self.set_backdrop_image(movie.backdrop)
 
     def close_action(self):
         self.close_details.emit()
